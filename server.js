b93ea4d64d91d68bb8b08346387e3d67bf3ce82c
Leo <leonadia1121@gmail.com>
2019-04-18 19:14:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  
 const app = express();  
-app.use(express.static(__dirname + '/dist'));  
+app.use(express.static(__dirname + '/dist/website-frontend'));  
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/website-frontend/index.html');  
 });  
 app.listen(process.env.PORT || 8080);  
